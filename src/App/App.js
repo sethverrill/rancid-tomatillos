83f35e91aea14b40cc458b4558c379c247792feb
sethverrill <verrill.seth@gmail.com>
2024-12-13 08:42:24 -0700

    Set up app with structure and Title layout working

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,32 @@
-import './App.css';
-import searchIcon from '../icons/search.png';
+import "./App.css";
+import searchIcon from "../icons/search.png";
 
 // Example imports (for later):
-// import { useState, useEffect } from 'react';
-// import moviePosters from '../data/movie_posters';
-// import movieDetails from '../data/movie_details';
-// import MoviesContainer from '../MoviesContainer/MoviesContainer';
+import { useState, useEffect } from "react";
+import moviePosters from '../data/movie_posters';
+import movieDetails from '../data/movie_details';
+import MoviesContainer from '../MoviesContainer/MoviesContainer';
+import MovieDetails from "../MovieDetails/MovieDetails";
 
 function App() {
+  const [selectedMovie, setMovies] = useState(null);
+  const movies = moviePosters.map((poster) => {
+    const detail = movieDetails.id === poster.id ? movieDetails : {}
+    return { ...poster, ...detail }
+  })
+
   return (
-    <main className='App'>
+    <main className="App">
       <header>
         <h1>rancid tomatillos</h1>
       </header>
+      {selectedMovie && (
+        <MovieDetails
+          selectedMovie={selectedMovie}
+          setMovies={setMovies}
+        />
+      )}
+      {!selectedMovie && <MoviesContainer movies={movies} setMovies={setMovies} />}
     </main>
   );
 }
