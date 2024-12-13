d524dcdb0b02d3cef33527e86535191ccaff56be
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-12-13 16:02:15 -0600

    added ability to create movies tiles from a file

--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -1,11 +1,20 @@
 import './MoviesContainer.css';
 
-function Movies() {
+
+function Movies({ moviePosters }) {
+  console.log('Type of moviePosters:', typeof moviePosters);
+  console.log('Is moviePosters an array:', Array.isArray(moviePosters));
+  console.log('Content of moviePosters:', moviePosters);
   return (
-      <section className='MoviesContainer'>
-        <p>We'll make some movie posters show up here!</p>
-      </section>
+    <section className='MoviesContainer'>
+      {moviePosters.map((movie) => (
+        <div key={movie.id}>
+          <img src={movie.poster_path} alt={`Poster for ${movie.id}`} />
+          <p>{movie.vote_count}</p>
+        </div>
+      ))}
+    </section>
   );
 }
-  
+
 export default Movies;
