8ac98549f5f5c33135891919f55840e9f51b9008
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-12-17 15:23:57 -0600

    added router refactor for movie details

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import "./MovieDetails.css";
+import { useParams } from "react-router-dom";
 
-function MovieDetails({ selectedMovie, setSelectedMovie }) {
-  if (!selectedMovie) {
+
+function MovieDetails() {
+  const [movieDetails, setMovieDetails] = useState(null);
+  
+  const movieId = useParams().id
+  useEffect(() => {
+    fetch(`https://rancid-tomatillos-api.onrender.com/api/v1/movies/${movieId}`, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setMovieDetails(data)
+      })
+      .catch((error) =>
+        console.error("Error fetching movie details:", error)
+      );
+  }, [movieId])
+  
+  if (!movieDetails) {
     return <p>Loading movie details...</p>;
   }
 
@@ -9,23 +30,23 @@ function MovieDetails({ selectedMovie, setSelectedMovie }) {
     <section className="MovieDetails">
       <figure className="MovieDetails-image">
         <img
-          src={selectedMovie.backdrop_path}
-          alt={`Backdrop for ${selectedMovie.title} `}
+          src={movieDetails.backdrop_path}
+          alt={`Backdrop for ${movieDetails.title} `}
           className="movie-image"
         />
       </figure>
       <section className="MovieDetails-content">
-        <h2 className="movie-title">{selectedMovie.title}</h2>
-        {selectedMovie.genre_ids && (
+        <h2 className="movie-title">{movieDetails.title}</h2>
+        {movieDetails.genre_ids && (
           <div className="genres">
-            {selectedMovie.genre_ids.map((genre, index) => (
+            {movieDetails.genre_ids.map((genre, index) => (
               <span key={index} className="genre-tag">
                 {genre}
               </span>
             ))}
           </div>
         )}
-        <p className="overview">{selectedMovie.overview}</p>
+        <p className="overview">{movieDetails.overview}</p>
       </section>
     </section>
   );
